b879ba513e0bfde0ec5ac0cad228435a4b719fdf
“knoweski <Krystle.noweski@gmail.com ~$ git config --global user.name knoweski>
2020-01-29 12:46:15 -0500

    problem number 3

--- drills.js
+++ drills.js
@@ -23,3 +23,34 @@ function binarySearch(array, value, start = 0, end = array.length - 1) {
 binarySearch(sortedList, 16);
 // 8 located at index of 3
 // 16 not included in list
+
+
+
+
+//3
+
+function deweySearching(dewey, title, start, end) {
+  start = start === undefined ? 0 : start;
+  end = end === undefined ? dewey.length : end;
+
+  if (start > end) {
+    return -1;
+  }
+  const index = Math.floor((start + end) / 2);
+  const item = dewey[index];
+
+  for (let i = 0; i < dewey.length; i++) {   
+    if (dewey[i] == title) {
+      return 'found book'
+    } 
+  }
+  if (item < dewey) {
+    return deweySearching(dewey, title, index + 1, end);
+  }
+  else if (item > dewey) {
+    return deweySearching(dewey, title, start, index - 1);
+  }
+}
+console.log(deweySearching(['be', 'he', 'hi'], 'hi'))
+
+//4
